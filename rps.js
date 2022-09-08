c3b57b0abefcd415d57a69fa43e76d19693a0e1c
Gregor <ggrbec@gmail.com>
2022-09-08 13:53:50 +0200

    Visuals done

--- rps.js
+++ rps.js
@@ -39,27 +39,27 @@ function determineOutcome(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  playerWinCount = 0;
-  computerWinCount = 0;
+// function game() {
+//   playerWinCount = 0;
+//   computerWinCount = 0;
 
-  while ((playerWinCount != 3) & (computerWinCount != 3)) {
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Write 'rock', 'paper' or 'scissors'");
+//   while ((playerWinCount != 3) & (computerWinCount != 3)) {
+//     computerSelection = getComputerChoice();
+//     playerSelection = prompt("Write 'rock', 'paper' or 'scissors'");
 
-    outcome = determineOutcome(playerSelection, computerSelection);
-    console.log(outcome);
-    if (outcome == "You lose!") {
-      computerWinCount += 1;
-    } else if (outcome == "You win!") {
-      playerWinCount += 1;
-    }
-    console.log("Computer wins: ", computerWinCount);
-    console.log("Player wins: ", playerWinCount);
+//     outcome = determineOutcome(playerSelection, computerSelection);
+//     console.log(outcome);
+//     if (outcome == "You lose!") {
+//       computerWinCount += 1;
+//     } else if (outcome == "You win!") {
+//       playerWinCount += 1;
+//     }
+//     console.log("Computer wins: ", computerWinCount);
+//     console.log("Player wins: ", playerWinCount);
 
-    if (computerWinCount == 3) console.log("Computer wins this game");
-    else if (playerWinCount == 3) console.log("Player wins this game");
-  }
-}
+//     if (computerWinCount == 3) console.log("Computer wins this game");
+//     else if (playerWinCount == 3) console.log("Player wins this game");
+//   }
+// }
 
-game();
+// game();
